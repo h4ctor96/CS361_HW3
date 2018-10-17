cce56fbb07c2b7a7ff9fdadfaae806d02420b41f
h4ctor96 <miticochiva@gmail.com>
2018-10-17 17:21:13 -0500

    Added string parsing

--- hw3.c
+++ hw3.c
@@ -1,13 +1,68 @@
 //this is the start of my hw3
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 
 
 main(){
 
+    
+    //create some space for our strings
+    char line[500];
+    char argsarray[20][100];
 
-    printf("361> ");
-    ///fgets
-    //
+    //print prompt
+    printf("361>");
+    //read line from terminal
+    fgets(line, 500, stdin);
+
+    while(line != "exit")
+    {
+        //break the string up into words
+        char *word = strtok(line, " ");
+        int i = 0;
+        while (word) {
+            printf("word: %s\n", word);
+            //copy a word to the arg array
+            strcpy(argsarray[i], word);
+            //get next word
+            word = strtok(NULL, " ");
+            i = i + 1;
+        }
+        //OPTIONAL: print out our array 
+        for (int j=0;j<i;j++){
+        printf("argsarray[%d]: %s\n", j, argsarray[j]);
+
+        // reading child's pid
+        int pid = fork();
+        if (pid == 0){
+            printf("PID: %d\n", getpid());
+            //exit;
+            //TODO: recognize command and execute it
+            execv(argsarray[0], argsarray);
+        }
+        else{
+            printf("Parent PID: %d\n", getpid());
+            int status;
+            wait(&status);
+            printf("EXIT: %d\n", WEXITSTATUS(status));
+        }
+
+        //TODO: set up file redirection
+        // dup2(fd1,0) sends output of fd1 to stdin
+
+
+
+
+
+        //print prompt for next command
+        printf("\n361>");
+        //read line from terminal
+        fgets(line, 500, stdin);
+    }
+    
+  }  
+
 }
