265006d55ed3c434d295e639a832ec1983483080
h4ctor96 <miticochiva@gmail.com>
2018-10-18 17:47:22 -0500

    fixed argsarray

--- hw3.c
+++ hw3.c
@@ -4,23 +4,26 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 void sigint_handler(int sig){
   char msg[] = "caught sigint\n";
   write(1, msg, sizeof(msg));
   //exit(0); // this should stop it from exiting
 }
-void sigstp_handler(int sig){
+void sigstop_handler(int sig){
   char msg[] = "caught sigstp\n";
   write(1, msg, sizeof(msg));
   //exit(0);
 }
 
-main(){
+int main(){
     //TODO: Ctrl+C triggers SIGINT, and Ctrl+Z triggers SIGSTP 
     //neither works
-    signal(SIGINT, sigint_handler);
-    signal(SIGSTP, sigstp_handler);
+    //signal(SIGINT, sigint_handler);
+    //signal(SIGTSTP, sigstop_handler);
 
     
     //create some space for our strings
@@ -31,25 +34,35 @@ main(){
     //read line from terminal
     fgets(line, 500, stdin);
 
-    while(line != "exit")
+    int length = strlen(line);
+    line[length-1] = '\0';
+
+    while(strcmp(line,"exit") != 0)
     {
         //break the string up into words
-        char argsarray[20][100];
+        char* argsarray[20];
+        int i = 0;
+        for(i=0; i < 20; ++i){
+            argsarray[i] = (char*)malloc(100*sizeof(char));
+            //printf("x");
+        }
+
         char* word = strtok(line, " ");
         char* filename;
-        int i = 0;
-        while (word) {//! need to add condition for < and >
-            printf("word: %s\n", word);
+        i = 0;
+        while (word) {
+            //printf("word: %s\n", word);
             //copy a word to the arg array
             strcpy(argsarray[i], word);
             //get next word
             word = strtok(NULL, " ");
             i = i + 1;
-            if(*word == '<' || *word == '>'){//word remains as symbol
-                filename = strtok(NULL, " ");
-                break;
-            }
+            //if(*word == '<' || *word == '>'){//word remains as symbol
+              //  filename = strtok(NULL, " ");
+                //break;
+            //}
         }
+        argsarray[i] = NULL;
         //OPTIONAL: print out array 
         for (int j=0;j<i;j++)
             printf("argsarray[%d]: %s\n", j, argsarray[j]);
@@ -57,32 +70,34 @@ main(){
         // reading child's pid
         int pid = fork();
         if (pid == 0){
-            printf("PID: %d\n", getpid());
-            //exit(0); //might want to execute later
+            //printf("PID: %d\n", getpid());
+            //exit(0); //exit for now, redirection might have to go here
             //TODO: recognize command and execute it
-            fd1 = execv(argsarray[0], argsarray);//!missing (char*)0
+            execvp(argsarray[0], argsarray);//!missing (char*)0
         }
         else{
-            printf("pid:%d status:&d\n", getpid(), 0);//?status?
             int status;
             wait(&status);
-            printf("EXIT: %d\n", WEXITSTATUS(status));
+            printf("pid:%d status:&d\n", pid, WEXITSTATUS(status));
+            //printf("EXIT: %d\n", WEXITSTATUS(status));
         }
-
+/*
         //TODO: set up file redirection
         // dup2(fd1,0) sends output of fd1 to stdin
-        if(filename){ //* this probably goes where the exec happens
+        if(filename){
             if(*word == '>'){ // add >>
-                fd2 = open(filename,O_CREAT);
+                int fd1 = exec(argsarray[0], argsarray);
+                int fd2 = open(filename,O_CREAT);
                 dup2(fd2, fd1);
-                //?should I use execv here?
+                
             }
             if(*word == '<'){
-                fd2 = open(filename,O_RDONLY);
-                dup2(fd1, fd2);
-                //?should I use execv here?
+                int fd2 = open(filename, O_RDONLY);
+                dup2(fd2, 0);
+                exec(argsarray[0], argsarray);
+
             }
-        }
+        }*/
 
 
         //print prompt for next command
@@ -91,6 +106,5 @@ main(){
         fgets(line, 500, stdin);
     }
     
-  }  
-
+    return 0;
 }
